0d176502cb072c92e916b7862cbc1c309ebf4248
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2022-10-21 04:19:05 -0300

    intento de arreglo de error al deployar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+
 import Header from "../../Components/Header/Header";
+
 import Nav from "../../Components/Nav/Nav";
-import About from "../About/About";
+
+import "./Home.css";
 import Projects from "../Projects/projects";
 import Skills from "../Skills/Skills";
-import "./Home.css";
+
 const Home = () => {
   return (
     <section className="cont-superior">
